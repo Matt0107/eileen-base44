6c7d5d7113b8edd00dc56f70c14763ffe7af1259
Matt0107 <mathieu.durimel@gmail.com>
2025-11-14 21:09:40 +0100

    footer-social added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { FaSpotify, FaYoutube, FaApple } from "react-icons/fa";
 import "../styles/Footer.css";
 
 export default function Footer() {
@@ -18,7 +19,8 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer-inner">
-        {/* Colonne 1 : nom */}
+
+        {/* Colonne 1 */}
         <div className="footer-col">
           <h3 className="footer-title">Eileen Baum</h3>
           <p className="footer-sub">
@@ -26,11 +28,12 @@ export default function Footer() {
           </p>
         </div>
 
-        {/* Colonne 2 : navigation */}
+        {/* Colonne 2 */}
         <div className="footer-col">
           <h4 className="footer-heading">
             {isDe ? "Navigation" : "Navigation"}
           </h4>
+
           <div className="footer-links">
             {navItems.map((item) => (
               <Link key={item.path} to={item.path} className="footer-link">
@@ -40,12 +43,45 @@ export default function Footer() {
           </div>
         </div>
 
-        {/* Colonne 3 : contact */}
+        {/* Colonne 3 : Contact + plateformes */}
         <div className="footer-col">
           <h4 className="footer-heading">
             {isDe ? "Kontakt" : "Contact"}
           </h4>
-          <p className="footer-text">info@eileenbaum.com</p>
+
+          <p className="footer-text">info@eileenbaum.de</p>
+
+          <div className="footer-social">
+            <a
+              href="https://www.youtube.com/channel/UCsezrcwS3afG6R4kYCGEiqg"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="footer-social-link"
+              aria-label="YouTube"
+            >
+              <FaYoutube className="footer-social-icon" />
+            </a>
+
+            <a
+              href="https://music.apple.com/gd/artist/eileen-baum/1497923452"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="footer-social-link"
+              aria-label="Apple Music"
+            >
+              <FaApple className="footer-social-icon" />
+            </a>
+
+            <a
+              href="https://open.spotify.com/artist/6BEnmlx54iiGEDYXS7jcN4"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="footer-social-link"
+              aria-label="Spotify"
+            >
+              <FaSpotify className="footer-social-icon" />
+            </a>
+          </div>
         </div>
       </div>
 
